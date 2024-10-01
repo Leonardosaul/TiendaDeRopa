373c39dd51a27ea9164122d1e5bc65b4e31ebcd1
Leonardo Saúl Alcalá Pachas <leitoEL456@gmail.com>
2024-09-30 22:50:21 -0500

    Creation if the fourth part of the DTO's

--- src/main/java/com/tiendaderopa/tiendaderopa/DTO/Request/Compra/DetalleCompra/SaveRequest.java
+++ src/main/java/com/tiendaderopa/tiendaderopa/DTO/Request/Compra/DetalleCompra/SaveRequest.java
@@ -2,4 +2,53 @@ package com.tiendaderopa.tiendaderopa.DTO.Request.Compra.DetalleCompra;
 
 public class SaveRequest {
     
+    private long compraId;
+    private long productoId;
+    private long proveedorId;
+    private int cantidad;
+    private double precioBruto;
+
+    public SaveRequest() {
+        
+    }
+
+    public long getCompraId() {
+        return this.compraId;
+    }
+
+    public void setCompraId(long compraId) {
+        this.compraId = compraId;
+    }
+
+    public long getProductoId() {
+        return this.productoId;
+    }
+
+    public void setProductoId(long productoId) {
+        this.productoId = productoId;
+    }
+
+    public long getProveedorId() {
+        return this.proveedorId;
+    }
+
+    public void setProveedorId(long proveedorId) {
+        this.proveedorId = proveedorId;
+    }
+
+    public int getCantidad() {
+        return this.cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioBruto() {
+        return this.precioBruto;
+    }
+
+    public void setPrecioBruto(double precioBruto) {
+        this.precioBruto = precioBruto;
+    }
 }
--- src/main/java/com/tiendaderopa/tiendaderopa/DTO/Request/Promocion/DetallePromocion/SaveRequest.java
+++ src/main/java/com/tiendaderopa/tiendaderopa/DTO/Request/Promocion/DetallePromocion/SaveRequest.java
@@ -2,4 +2,35 @@ package com.tiendaderopa.tiendaderopa.DTO.Request.Promocion.DetallePromocion;
 
 public class SaveRequest {
     
+    private long promocionId;
+    private long productoId;
+    private long categoriaProductoId;
+
+    public SaveRequest() {
+        
+    }
+
+    public long getPromocionId() {
+        return this.promocionId;
+    }
+
+    public void setPromocionId(long promocionId) {
+        this.promocionId = promocionId;
+    }
+
+    public long getProductoId() {
+        return this.productoId;
+    }
+
+    public void setProductoId(long productoId) {
+        this.productoId = productoId;
+    }
+
+    public long getCategoriaProductoId() {
+        return this.categoriaProductoId;
+    }
+
+    public void setCategoriaProductoId(long categoriaProductoId) {
+        this.categoriaProductoId = categoriaProductoId;
+    }
 }
